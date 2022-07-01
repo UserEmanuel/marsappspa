55099d15fdcedd7738428876aebae12c5922b265
EmanuelDicu <Emanuel.Dicu@softwire.com>
2022-07-01 09:07:41 +0100

    Commit tasks 1, 2, 3

---
+++ src/App.tsx
@@ -0,0 +1,112 @@
+import React from 'react';
+
+
+import { useState, useContext, createContext } from "react";
+import logo from './logo.svg';
+import nasalogo from './r.png'
+import './App.css';
+
+type NewType = [number, (a: number) => void];
+export const CounterContext = createContext<NewType>(undefined as unknown as NewType);
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <TopLevelComponent
+                    firstParagraph="This is the first paragraph"
+                    secondParagraph="This is the second paragraph"
+                    imageLink={nasalogo}
+                />
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+}
+
+function TopLevelComponent(props: {firstParagraph: string; secondParagraph: string, imageLink: string}) {
+    const [count, setCount] = useState(parseInt(localStorage.getItem('count') as string));
+
+    const handleClick = (num: number) => {
+        localStorage.setItem('count', num.toString());
+        setCount(num);
+    }
+
+    return (
+        <CounterContext.Provider value={[count, handleClick]}>
+            <div className="TopLevelComponent">
+                <title>NASA is cool</title>
+                <body>
+                <p>{props.firstParagraph}</p>
+                <p>{props.secondParagraph}</p>
+                <img src={props.imageLink} alt="nasa" width={100}/>
+                <ButtonComponent/>
+                <CounterComponent/>
+                </body>
+            </div>
+        </CounterContext.Provider>
+    );
+}
+
+function ButtonComponent() {
+    const [count, handleClick] = useContext(CounterContext);
+
+    return (
+        <div className="ButtonComponent">
+            <button onClick={() => handleClick(count + 1)}>
+                Click here
+            </button>
+        </div>
+    );
+}
+
+function CounterComponent() {
+    const [count, ] = useContext(CounterContext);
+
+    return (
+        <div className="CounterComponent">
+            <p>You clicked {count} times</p>
+        </div>
+    );
+}
+
+function NasaData(props: {firstParagraph: string; secondParagraph: string, imageLink: string}) {
+    return (
+        <div className="NasaData">
+            <title>NASA is cool</title>
+            <body>
+            <p>{props.firstParagraph}</p>
+            <p>{props.secondParagraph}</p>
+            <img src={props.imageLink} alt="nasa" width={100}/>
+            </body>
+        </div>
+    );
+}
+
+function Counter() {
+    const [count, setCount] = useState(parseInt(localStorage.getItem('count') as string));
+
+    const handleClick = (num: number) => {
+        localStorage.setItem('count', num.toString());
+        setCount(num);
+    }
+
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => handleClick(count + 1)}>
+                Click here
+            </button>
+        </div>
+    );
+}
+
+export default App;
